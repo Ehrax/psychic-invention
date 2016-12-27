c4ea1c677c85569f89dd7785f41a3869eeac4225
Jona Ruof <jona.ruof@uni-ulm.de>
2016-12-27 20:03:55 +0100

    cleanup in MainMenuActivity

--- Quartett/app/src/main/java/de/in/uulm/map/quartett/mainmenu/MainMenuActivity.java
+++ Quartett/app/src/main/java/de/in/uulm/map/quartett/mainmenu/MainMenuActivity.java
@@ -20,49 +20,7 @@ public class MainMenuActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-
-        /**
-
-        StringWriter stringWriter = new StringWriter();
-
-        try(InputStream in = getResources().openRawResource(R.raw.tuning)) {
-
-            BufferedReader reader =
-                    new BufferedReader(new InputStreamReader(in));
-
-            char[] buffer = new char[1024];
-
-            int n;
-            while ((n = reader.read(buffer)) != -1) {
-                stringWriter.write(buffer, 0, n);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        EntityFactory entityFactory = new EntityFactory(this);
-
-        Deck deck = null;
-
-        try {
-            deck = entityFactory.getDeck(new JSONObject(stringWriter.toString()));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-         */
-
-        EntityFactory entityFactory = new EntityFactory(this);
-
-        try {
-            entityFactory.getDeckFromAssets("decks/bikes/bikes.json");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
