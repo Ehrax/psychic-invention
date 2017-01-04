e62b01edf1af8c97f0aaf76d7e6fb5c262d44f80
Max Karthan <maximilian.karthan@uni-ulm.de>
2017-01-04 21:41:04 +0100

    Gallery and Deck view now working with new data model. But Card textview styles have no effect when set programmatically

---
+++ Quartett/app/src/main/java/de/in/uulm/map/quartett/util/AssetUtils.java
@@ -0,0 +1,17 @@
+package de.in.uulm.map.quartett.util;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+
+/**
+ * Created by maxka on 04.01.2017.
+ */
+
+public class AssetUtils {
+
+    public static Drawable getDrawableFromAssetUri(Context ctx, Uri assetUri){
+
+    }
+
+}
