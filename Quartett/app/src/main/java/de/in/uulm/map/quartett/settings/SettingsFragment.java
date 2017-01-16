acab4dbe5b25943e8c2235118d6d49c0788f1f7f
Alexander Rasputin <alexanderrasputin@wlan139-057.wlan.uni-ulm.de>
2017-01-16 17:30:27 +0100

    hotfix for drawer_header, name is now correctly displayed

--- Quartett/app/src/main/java/de/in/uulm/map/quartett/settings/SettingsFragment.java
+++ Quartett/app/src/main/java/de/in/uulm/map/quartett/settings/SettingsFragment.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.preference.EditTextPreference;
 import android.support.v7.preference.ListPreference;
